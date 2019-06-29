f608ba071a36f1dc789a55c41aa4147232c0fed7
Jarele Soyinka <t-jaso@microsoft.com>
2019-06-29 11:42:44 -0700

    Ignore Files

--- ghunzip.py
+++ ghunzip.py
@@ -10,18 +10,15 @@ import json
 files = os.listdir()
 types = []
 json_file = []
-for name in files:
-    if "json.gz" in name:
-        with gzip.open() as f:
-            for i, line in enumerate(f):
-                json_data = ujson.loads(line)
-                json_file.append(json_data)
-            f.close()
-        new_name = name.replace("json.gz", "json")
-        with open(new_name, 'w') as fp:
-            json.dump(json_file, fp)
-        print(new_name)
+# for name in files:
+#     if "json.gz" in name:
+with gzip.open("2016-01-01-0.json.gz") as f:
+    for i, line in enumerate(f):
+        json_file.append(ujson.loads(line))
+    f.close()
+# new_name = name.replace("json.gz", "json")
+with open("2016-01-01-0.json", 'w') as fp:
+    json.dump(json_file, fp, indent=2)
+# print(new_name)
 
-print(data)
-
-
+# print(data)
